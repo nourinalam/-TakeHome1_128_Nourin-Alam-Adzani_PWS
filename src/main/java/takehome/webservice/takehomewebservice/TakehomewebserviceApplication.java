de995f707496f910d27bc4b0f682c62c72e3a6d3
nourinalam <nourinalam26@gmail.com>
2022-10-30 13:01:44 +0700

    membuat project

---
+++ src/main/java/takehome/webservice/takehomewebservice/TakehomewebserviceApplication.java
@@ -0,0 +1,13 @@
+package takehome.webservice.takehomewebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TakehomewebserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TakehomewebserviceApplication.class, args);
+	}
+
+}
